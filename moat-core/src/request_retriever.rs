154a0ee35a08f4b4d7e69feaf80d05e239f81ac0
Milosz Muszynski <milosz.muszynski@gmail.com>
2023-06-14 12:25:39 +0200

    Retrieval of encoded call data works

--- moat-core/src/request_retriever.rs
+++ moat-core/src/request_retriever.rs
@@ -20,11 +20,26 @@ struct ContractInfo {
 struct Tx {
     pub txid: String,
     pub contractinfo: ContractInfo,
+    pub json: String,
 }
 
 #[derive(Debug, serde::Deserialize)]
 struct Transactions {
     pub transactions: Vec<Tx>,
+}
+
+
+#[derive(Debug, Default, serde::Deserialize, serde::Serialize)]
+struct CallInfoJson {
+    pub ContractID: String,
+    pub FnName: String,
+    pub CallData: String,
+}
+
+#[derive(Debug, Default, serde::Deserialize, serde::Serialize)]
+struct TxJson {
+    pub anchor: String,
+    pub call: CallInfoJson,
 }
 
 impl RequestRetriever {
@@ -34,11 +49,17 @@ impl RequestRetriever {
         let txid= "61f978ec921ff4da4d2a73e1424e5a251f508228f5243630132ec2f1a876b305";
 
         let query =
-            "{transactions(txid:\"####\"){ txid, contractinfo{method, contract}}}".replace("####", txid);
+            "{transactions(txid:\"####\"){ txid, contractinfo{method, contract}, json}}".replace("####", txid);
 
-        let response = client.query::<Transactions>(&query).await;
+        let response = client.query::<Transactions>(&query).await.expect("todo:");// todo: remove expect and replace with ?
+
+        let tx_json: TxJson = serde_json::from_str(response.as_ref().unwrap().transactions.get(0).unwrap().json.as_str()).expect("json conversion should work");
+
+        let request_encoded = tx_json.call.CallData.clone();
 
         println!("resp={:?}", response);
+        println!("tx_json={:?}", tx_json);
+        println!("request_encoded={:?}", request_encoded);
 
         Ok(())
     }
