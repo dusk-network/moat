f3c23443119b3eaa342e571d9fc98a3a811bb70d
Milosz Muszynski <milosz.muszynski@gmail.com>
2023-09-04 15:13:29 +0200

    Migrated scan and retrieve tests

--- integration-tests/tests/retrieve_requests.rs
+++ integration-tests/tests/retrieve_requests.rs
@@ -4,55 +4,56 @@
 //
 // Copyright (c) DUSK NETWORK. All rights reserved.
 
+use dusk_wallet::RuskHttpClient;
 use gql_client::Client;
 use moat_core::{Error, PayloadRetriever, RequestScanner};
 use toml_base_config::BaseConfig;
 use wallet_accessor::BlockchainAccessConfig;
 use zk_citadel::license::Request;
 
-// #[tokio::test(flavor = "multi_thread")]
-// #[ignore]
-// async fn retrieve_payload() -> Result<(), Error> {
-//     const TXID: &str =
-//         "5f486c6f4edc9321e15a83993aa68463e733fc482acbde979881450c83c92a0e";
-//
-//     let config_path =
-//         concat!(env!("CARGO_MANIFEST_DIR"), "/tests/config/config.toml");
-//
-//     let config = BlockchainAccessConfig::load_path(config_path)?;
-//     let client = Client::new(config.graphql_address.clone());
-//
-//     let request: Request =
-//         PayloadRetriever::retrieve_payload(TXID, &client).await?;
-//     println!("request={:?}", request);
-//     Ok(())
-// }
+#[tokio::test(flavor = "multi_thread")]
+#[cfg_attr(not(feature = "vol_tests"), ignore)]
+async fn retrieve_payload() -> Result<(), Error> {
+    const TXID: &str =
+        "8d45a9fb7196f322282d522ff4bb2d2e926ddd96b858b91d59f228b27250ef03";
 
-// #[tokio::test(flavor = "multi_thread")]
-// #[cfg_attr(not(feature = "exp_tests"), ignore)]
-// async fn scan_all_requests() -> Result<(), Error> {
-//     let config_path =
-//         concat!(env!("CARGO_MANIFEST_DIR"), "/tests/config/config.toml");
-//     let cfg = BlockchainAccessConfig::load_path(config_path)?;
-//     let mut height = 0;
-//     loop {
-//         let height_end = height + 10000;
-//         let (requests, top) =
-//             RequestScanner::scan_block_range(height, height_end,
-// &cfg).await?;         println!(
-//             "{} requests in range ({},{}) top={}",
-//             requests.len(),
-//             height,
-//             height_end,
-//             top
-//         );
-//         if top <= height_end {
-//             break;
-//         }
-//         height = height_end;
-//     }
-//     Ok(())
-// }
+    let config_path =
+        concat!(env!("CARGO_MANIFEST_DIR"), "/tests/config/config.toml");
+
+    let config = BlockchainAccessConfig::load_path(config_path)?;
+    let client = RuskHttpClient::new(config.rusk_address);
+
+    let request: Request =
+        PayloadRetriever::retrieve_payload(TXID, &client).await?;
+    println!("request={:?}", request);
+    Ok(())
+}
+
+#[tokio::test(flavor = "multi_thread")]
+#[cfg_attr(not(feature = "exp_tests"), ignore)]
+async fn scan_all_requests() -> Result<(), Error> {
+    let config_path =
+        concat!(env!("CARGO_MANIFEST_DIR"), "/tests/config/config.toml");
+    let cfg = BlockchainAccessConfig::load_path(config_path)?;
+    let mut height = 0;
+    loop {
+        let height_end = height + 10000;
+        let (requests, top) =
+            RequestScanner::scan_block_range(height, height_end, &cfg).await?;
+        println!(
+            "{} requests in range ({},{}) top={}",
+            requests.len(),
+            height,
+            height_end,
+            top
+        );
+        if top <= height_end {
+            break;
+        }
+        height = height_end;
+    }
+    Ok(())
+}
 
 #[tokio::test(flavor = "multi_thread")]
 #[cfg_attr(not(feature = "int_tests"), ignore)]
@@ -73,24 +74,24 @@ async fn scan_requests_in_last_blocks() -> Result<(), Error> {
     Ok(())
 }
 
-// #[tokio::test(flavor = "multi_thread")]
-// #[cfg_attr(not(feature = "int_tests"), ignore)]
-// async fn scan_requests_in_block_range() -> Result<(), Error> {
-//     let config_path =
-//         concat!(env!("CARGO_MANIFEST_DIR"), "/tests/config/config.toml");
-//
-//     let cfg = BlockchainAccessConfig::load_path(config_path)?;
-//
-//     const HEIGHT_BEG: u64 = 317042;
-//     const HEIGHT_END: u64 = 317048;
-//
-//     let (requests, _) =
-//         RequestScanner::scan_block_range(HEIGHT_BEG, HEIGHT_END,
-// &cfg).await?;     println!(
-//         "there were {} requests found in block range from {} to {}",
-//         requests.len(),
-//         HEIGHT_BEG,
-//         HEIGHT_END
-//     );
-//     Ok(())
-// }
+#[tokio::test(flavor = "multi_thread")]
+#[cfg_attr(not(feature = "int_tests"), ignore)]
+async fn scan_requests_in_block_range() -> Result<(), Error> {
+    let config_path =
+        concat!(env!("CARGO_MANIFEST_DIR"), "/tests/config/config.toml");
+
+    let cfg = BlockchainAccessConfig::load_path(config_path)?;
+
+    const HEIGHT_BEG: u64 = 0;
+    const HEIGHT_END: u64 = 20000;
+
+    let (requests, _) =
+        RequestScanner::scan_block_range(HEIGHT_BEG, HEIGHT_END, &cfg).await?;
+    println!(
+        "there were {} requests found in block range from {} to {}",
+        requests.len(),
+        HEIGHT_BEG,
+        HEIGHT_END
+    );
+    Ok(())
+}
