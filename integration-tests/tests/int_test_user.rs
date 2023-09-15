66054515fcaf97bfe5a9fca93d63369b5bcc57f2
Milosz Muszynski <milosz.muszynski@gmail.com>
2023-09-15 16:09:09 +0200

    Second call to use_license

--- integration-tests/tests/int_test_user.rs
+++ integration-tests/tests/int_test_user.rs
@@ -251,5 +251,9 @@ async fn user_round_trip() -> Result<(), Error> {
     let session = session.unwrap();
     println!("obtained session {}", hex::encode(session.public_inputs[0].to_bytes()));
 
+    // as a User, try to call use_license again, should it be rejected?
+    show_state(&client, "before second use_license").await?;
+    use_license(&client, &blockchain_config, &wallet_path, &reference_lp, ssk_user, &prover, &verifier, &license, opening.unwrap(), rng).await?;
+    show_state(&client, "after second use_license").await?;
     Ok(())
 }
